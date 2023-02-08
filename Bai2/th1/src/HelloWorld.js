7d5fc8212ca266234ffdd7b4e1efd75354b6e380
phamvandureactjs <phamvandu.uet.vnu@gmail.com>
2023-02-08 20:24:47 +0700

    TH1

---
+++ Bai2/th1/src/HelloWorld.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HelloWorld(){
+    let name = 'Cee';
+    const user = {
+        firstName : 'Dự',
+        lastName : 'Phạm Văn'
+    };
+    const formatName = (user) =>{
+        return user.lastName + ' ' + user.firstName;
+    }
+    return (
+        <div>
+           <div>Xin chào {name } và {formatName(user)}</div>
+            <div>Rất vui khi được gặp bạn</div>
+        </div>
+    );
+}
